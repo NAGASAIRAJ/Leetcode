e7b73663634f0a2530070e0aff0d3ab58257604e
Yao <myspiritcrazy@gmail.com>
2013-11-27 16:36:50 -0500

    try in bit operation

--- src/SingleNumber.java
+++ src/SingleNumber.java
@@ -3,17 +3,14 @@ public class SingleNumber {
     public static int singleNumber(int[] A) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
-    	int half1 = A[A.length/2];
-    	int half2 = 0;
-    	for( int i = 0; i < A.length/2; i++ ) {
-    		half1 += A[i];
-    		half2 += A[A.length-1-i];
-    	}        
-        return (half1 - half2);
+    	int result = A[0];    	
+    	for( int i = 1; i < A.length; i++ ) 
+    		result ^= A[i];
+        return result;
     }
 	
 	public static void main(String[] args) {
-		int[] test = {1, 2, 3};
+		int[] test = {1, 2, 1};
 		System.out.println("Unique number: " + singleNumber(test));
 	}	
 
