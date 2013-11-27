0820848feec6fbab78916186ceffb7b4fa7d816d
Yao <myspiritcrazy@gmail.com>
2013-11-27 14:40:27 -0500

    first try on Linked List Cycle 1

---
+++ src/LinkedListCycle.java
@@ -0,0 +1,39 @@
+
+public class LinkedListCycle {
+    public static boolean hasCycle(ListNode head) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+    	if( head == null || head.next == null )
+    		return false;
+    	ListNode slow = head;
+    	ListNode fast = head;
+    	while( fast != null || fast.next != null ) {
+    		slow = slow.next;
+    		fast = fast.next.next;
+    		if( slow == fast )
+    			return true;
+    	}
+    	return false;
+    }
+    
+	public static void main(String[] args) {
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = new ListNode(4);
+		
+		if( hasCycle(head))
+			System.out.println("Linked List has cycle!");
+		else
+			System.out.println("Linked List has no cycle!");
+	}
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
