36c428a87e967d9fb0c6b6de2276f581404b8c3f
Yao <myspiritcrazy@gmail.com>
2013-11-29 16:39:41 -0500

    return 0 when there is no price now

--- src/BestTimetoBuyandSellStock.java
+++ src/BestTimetoBuyandSellStock.java
@@ -1,6 +1,7 @@
 
 public class BestTimetoBuyandSellStock {
 	public static int maxProfit(int[] prices) {
+		if( prices.length == 0 ) return 0;
 		int min_price = prices[0];
 		int profit = 0;
 		for( int i = 0; i < prices.length; i++ ) {
