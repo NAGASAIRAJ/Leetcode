a9a1107693b2eddc981fab83516b71d73e82122d
Yao <myspiritcrazy@gmail.com>
2013-12-02 16:42:53 -0500

    add a test case with only one elem

--- src/RemoveElement.java
+++ src/RemoveElement.java
@@ -35,9 +35,11 @@ public class RemoveElement {
 		int[] test2 = {1, 1, 2};
 		int[] test3 = {1, 1, 1, 2};
 		int[] test4 = {1, 1};
+		int[] test5 = {1, 2, 3, 4};
 		System.out.println("No Duplicate Lenth in test case 1: " + removeElement(test, 2));
 		System.out.println("No Duplicate Lenth in test case 2: " + removeElement(test2, 1));
 		System.out.println("No Duplicate Lenth in test case 3: " + removeElement(test3, 1));
-		System.out.println("No Duplicate Lenth in test case 3: " + removeElement(test4, 1));
+		System.out.println("No Duplicate Lenth in test case 4: " + removeElement(test4, 1));
+		System.out.println("No Duplicate Lenth in test case 5: " + removeElement(test5, 1));
     }
 }
