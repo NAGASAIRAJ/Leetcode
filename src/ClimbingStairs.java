a108938f27a89f188dcf1b5a714b1ca1eaf78fcb
Yao <myspiritcrazy@gmail.com>
2013-12-02 15:25:53 -0500

    first try on Climbing Stairs with iterative way

---
+++ src/ClimbingStairs.java
@@ -0,0 +1,20 @@
+
+public class ClimbingStairs {
+    public static int climbStairs(int n) {
+    	if( n == 0 || n == 1 )
+    		return 1;
+    	if( n == 2 ) 
+    		return 2;
+    	int[] tmp = new int[n];
+    	tmp[0] = 1;
+    	tmp[1] = 2;
+    	for( int i = 2; i < n; i++ )
+    		tmp[i] = tmp[i-1] + tmp[i-2];
+    	return tmp[n-1];
+    }
+    
+    public static void main(String[] args) {
+    	System.out.println("Climb 4 stairs ways in iterative: " + climbStairs(4));
+    	System.out.println("Climb 4 stairs ways in recusive: " + climbStairs(4));
+    }
+}
