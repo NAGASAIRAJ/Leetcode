0d21300ec847c1775c1134811759ecb425eba257
Yao <myspiritcrazy@gmail.com>
2013-12-02 15:28:23 -0500

    solve Climbing Stairs with recursion

--- src/ClimbingStairs.java
+++ src/ClimbingStairs.java
@@ -12,9 +12,18 @@ public class ClimbingStairs {
     		tmp[i] = tmp[i-1] + tmp[i-2];
     	return tmp[n-1];
     }
+
+    public static int climbStairs_recursion(int n) {
+    	if( n == 0 || n == 1 )
+    		return 1;
+    	if( n == 2 ) 
+    		return 2;
+    	
+    	return climbStairs_recursion(n-1) + climbStairs_recursion(n-2);
+    }
     
     public static void main(String[] args) {
     	System.out.println("Climb 4 stairs ways in iterative: " + climbStairs(4));
-    	System.out.println("Climb 4 stairs ways in recusive: " + climbStairs(4));
+    	System.out.println("Climb 4 stairs ways in recusive: " + climbStairs_recursion(4));
     }
 }
