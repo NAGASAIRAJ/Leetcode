441fe3456f26019440c609c7ee1764785722336c
Yao <myspiritcrazy@gmail.com>
2013-12-04 11:38:43 -0500

    scan backward to Merge Sorted Array

---
+++ src/MergeSortedArray.java
@@ -0,0 +1,26 @@
+
+public class MergeSortedArray {
+    public void merge(int A[], int m, int B[], int n) {
+    	int A_cur = m - 1;
+    	int B_cur = n - 1;
+    	int result_cur = m + n - 1; 
+    	while( A_cur >= 0 && B_cur >= 0 ) {
+    		if( A[A_cur] < B[B_cur] ) {
+    			A[result_cur] = A[A_cur];
+    			A_cur--;
+    			result_cur--;
+    		}
+    		else {
+    			A[result_cur] = B[B_cur];
+    			B_cur--;
+    			result_cur--;
+    		}
+    	}
+    	if( B_cur >= 0 ) {
+    		for( int i = B_cur; i >= 0; i-- ) {
+    			A[result_cur] = B[B_cur];
+    			result_cur--;
+    		}
+    	}    	
+    }
+}
