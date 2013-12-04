fbad74d0d73e7e7c0af0ef2ae7516c398e75bfee
Yao <myspiritcrazy@gmail.com>
2013-12-04 15:53:39 -0500

    second method to recursively generate Gray Code

--- src/GrayCode.java
+++ src/GrayCode.java
@@ -8,4 +8,19 @@ public class GrayCode {
     		result.add(i^(i>>1));
     	return result;
     }
+    
+    public ArrayList<Integer> grayCode2(int n) {    	
+    	ArrayList<Integer> result = new ArrayList<Integer>();
+    	result.add(0);
+    	if( n == 0 )     	
+    		return result;
+    	result.add(1);
+    	if( n == 1 )     		
+    		return result;
+    	ArrayList<Integer> prev = grayCode2(n-1);
+    	int top = 1 << n;
+    	for( int i = 0; i < prev.size(); i++ )
+    		result.add(top+prev.get(i));
+    	return result;
+    }
 }
