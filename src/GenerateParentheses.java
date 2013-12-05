ba523d59d1735598d11161738c71d7bcdca65b5d
Yao <myspiritcrazy@gmail.com>
2013-12-05 15:03:56 -0500

    recursively generate parenthesis

---
+++ src/GenerateParentheses.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+
+public class GenerateParentheses {
+    public ArrayList<String> generateParenthesis(int n) {
+    	ArrayList<String> results = new ArrayList<String>();
+    	StringBuffer tmp = new StringBuffer();
+    	generateParenthesis(3, 3, tmp, results);
+    	return results;
+    }
+    public void generateParenthesis(int left, int right, 
+    		StringBuffer cur, ArrayList<String> results) {
+    	if( right == 0 ) {
+    		results.add(cur.toString());
+    		return;
+    	}
+    	if( left > 0 ) {
+    		cur.append("{");
+			generateParenthesis(left-1, right, cur, results);
+    		if( left < right ) {
+    			cur.append("}");
+    			generateParenthesis(left, right-1, cur, results);
+    		}    		
+    	}
+    	else {
+    		cur.append("}");
+    		generateParenthesis(left, right-1, cur, results);
+    	}
+    }
+}
