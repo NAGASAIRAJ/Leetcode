6795f827f0759f784037f7ce88922935fca17ec3
Yao <myspiritcrazy@gmail.com>
2013-12-05 11:23:41 -0500

    first try Container With Most Water

---
+++ src/ContainerWithMostWater.java
@@ -0,0 +1,18 @@
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+    	int container, max = 0, start = 0, end = height.length;
+    	
+    	while( start < end ) {
+    		container = (end - start) * Math.min(height[start], height[end]);
+    		if( container > max )
+    			max = container;
+    		if( height[start] < height[end] )
+    			start++;
+    		else 
+    			end--;
+    	}
+    	
+    	return max;
+    }
+}
