c75d493e818afa3f5c2c472b7175c1f688c2766e
Yao <myspiritcrazy@gmail.com>
2013-12-05 11:36:28 -0500

    no target found when matrix is empty

--- src/Searcha2DMatrix.java
+++ src/Searcha2DMatrix.java
@@ -1,6 +1,8 @@
 
 public class Searcha2DMatrix {
-	public boolean searchMatrix(int[][] matrix, int target) {		  		
+	public boolean searchMatrix(int[][] matrix, int target) {		  
+		if( matrix.length == 0 || matrix[0].length == 0 )
+			return false;
 		int r = matrix.length - 1; 
 		int c = 0; 
 		while( r >= 0 && c < matrix[0].length ) {
