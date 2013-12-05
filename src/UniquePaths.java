d239e770c692b81c60217ce4ca6fc613ea323bce
Yao <myspiritcrazy@gmail.com>
2013-12-04 22:26:47 -0500

    recursively find unique paths top down

--- src/UniquePaths.java
+++ src/UniquePaths.java
@@ -14,4 +14,23 @@ public class UniquePaths {
     	}
     	return paths[1][1];
     }
+    
+    public int uniquePaths2(int m, int n) {
+    	int paths[][] = new int[m+2][n+2];
+    	for( int i = 0; i < m+2; i++ ) {
+    		for( int j = 0; j < n+2; j++ )
+    			paths[i][j] = -1;
+    	}
+    	return count_path(1, 1, m, n, paths);
+    }
+    
+    public int count_path(int r, int c, int m, int n, int[][] paths) {
+    	if( r > m || c > n )	return 0;
+    	if( r == m && c == n )	return 1;
+    	if( paths[r+1][c] == -1 )
+    		paths[r+1][c] = count_path(r+1, c, m, n, paths);
+    	if( paths[r][c+1] == -1 )
+    		paths[r][c+1] = count_path(r, c+1, m, n, paths);
+    	return  (paths[r+1][c] + paths[r][c+1]);
+    }
 }
