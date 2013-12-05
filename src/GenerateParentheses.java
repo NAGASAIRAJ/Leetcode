9b5ed0668fe9c744be1bbda68e01f652c67ea145
Yao <myspiritcrazy@gmail.com>
2013-12-05 15:31:08 -0500

    use String rather than StringBuffter

--- src/GenerateParentheses.java
+++ src/GenerateParentheses.java
@@ -5,30 +5,29 @@ public class GenerateParentheses {
     public ArrayList<String> generateParenthesis(int n) {
     	ArrayList<String> results = new ArrayList<String>();
     	if( n <= 0 ) return results;
-    	StringBuffer tmp = new StringBuffer();
-    	generateParenthesis(n, n, tmp, results);
+    	generateParenthesis(n, n, "", results);
     	return results;
     }
     public void generateParenthesis(int left, int right, 
-    		StringBuffer cur, ArrayList<String> results) {
+    		String cur, ArrayList<String> results) {
     	if( right == 0 ) {
-    		results.add(cur.toString());
+    		results.add(cur);
     		return;
     	}
     	if( left > 0 ) {
-    		cur.append("(");
+    		cur += "(";
 			generateParenthesis(left-1, right, cur, results);
     		if( left < right ) {
-    			cur.append(")");
+    			cur += ")";
     			generateParenthesis(left, right-1, cur, results);
     		}    		
     	}
     	else {
     		while( right > 0 ) {
-    			cur.append(")");
+    			cur += ")";
     			right--;
     		}
-    		results.add(cur.toString());
+    		results.add(cur);
     	}
     }
 }
