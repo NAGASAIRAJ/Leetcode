d4bd2eef2a4c570029500aa9eeefe1cf8d71aad6
Yao <myspiritcrazy@gmail.com>
2013-12-06 16:20:09 -0500

    use 2 pointers to scan forward once to sort colors

---
+++ src/SortColors.java
@@ -0,0 +1,27 @@
+
+public class SortColors {
+    public void sortColors(int[] A) {
+    	int len = A.length;
+    	if( len == 0 ) return;
+    	int i = 0, RedIndex = 0, BlueIndex = len - 1;
+    	while( i < BlueIndex + 1 ) {
+    		if( A[i] == 0 ) {
+    			swap(RedIndex, i, A);
+    			RedIndex++;
+    			i++;
+    		}
+    		else if( A[i] == 2 ) {
+    			swap(i, BlueIndex, A);
+    			BlueIndex--;
+    			i++;
+    		}
+    		else 
+    			i++;
+    	}
+    }
+    public void swap(int m, int n, int[] nums) {
+    	int tmp = nums[m];
+    	nums[m] = nums[n];
+    	nums[n] = tmp;
+    }
+}
