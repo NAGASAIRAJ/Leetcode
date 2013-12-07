a549bf60c0a81582c217eb1a143ff94e8810aee6
Yao <myspiritcrazy@gmail.com>
2013-12-06 21:24:39 -0500

    dont increase iterator when swap blue color

--- src/SortColors.java
+++ src/SortColors.java
@@ -4,36 +4,15 @@ public class SortColors {
     	int len = A.length;
     	if( len == 0 ) return;
     	int i = 0, RedIndex = 0, BlueIndex = len - 1;
-    	while( i < len ) {
+    	while( i < BlueIndex + 1 ) {
     		if( A[i] == 0 ) {
-    			if( i == 0 ) {
-    				i++;
-    				RedIndex++;
-    			}
-    			else if( A[RedIndex] != 0 ) {
-    				swap(RedIndex, i, A);
-    				RedIndex++;
-    				if( i < RedIndex )
-    					i = RedIndex;
-    			}
-    			else {
-    				RedIndex++;
-    				if( RedIndex >= len)	// [0, 0] case
-    					return;
-    			}
+    			swap(RedIndex, i, A);
+    			RedIndex++;
+    			i++;
     		}
     		else if( A[i] == 2 ) {
-    			if( i == A.length - 1 )
-    				return;
-    			else if( A[BlueIndex] != 2 ) {
-    				swap(i, BlueIndex, A);
-    				BlueIndex--;
-    			}
-    			else {
-    				BlueIndex--;
-    				if( BlueIndex < i )
-    					return;
-    			}
+    			swap(i, BlueIndex, A);
+    			BlueIndex--;
     		}
     		else 
     			i++;
