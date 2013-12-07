c3174d0f1f3585d07a1e0d2fdbf5313379e2a3c5
Yao <myspiritcrazy@gmail.com>
2013-12-07 16:41:57 -0500

    jump if meet the duplicated elements and recursively find Subsets 2

---
+++ src/Subsets2.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+
+public class Subsets2 {
+    public ArrayList<ArrayList<Integer>> subsetsWithDup(int[] num) {
+    	ArrayList<ArrayList<Integer>> subsets = new ArrayList<ArrayList<Integer>>();
+    	if( num.length == 0 )
+    		return subsets;
+    	GenerateSubset(0, num.length, num, new ArrayList<Integer>(), subsets);
+    	return subsets;
+    }
+    private void GenerateSubset(int depth, int len, int[] num, ArrayList<Integer> subset, 
+    							ArrayList<ArrayList<Integer>> subsets) {
+    	subsets.add(subset);
+    	if( depth == len )	return;
+    	for( int i = depth; i < len; i++ ) {
+    		ArrayList<Integer> TmpSubset = new ArrayList<Integer>();
+    		TmpSubset.addAll(subset);
+    		TmpSubset.add(num[i]);
+    		GenerateSubset(i + 1, len, num, TmpSubset, subsets);
+    		while( i + 1 < len && num[i] == num[i + 1] )
+    			i++;
+    	}
+    }
+}
