7ed995eaf0deb15ae399bd135caaa069e1f0135a
Yao <myspiritcrazy@gmail.com>
2013-12-07 15:50:09 -0500

    second method to use Current Touch Max Distance to detect touching current index or not

--- src/JumpGame.java
+++ src/JumpGame.java
@@ -12,4 +12,17 @@ public class JumpGame {
     	}
     	return true;
     }
+    public boolean canJump2(int[] A) {
+    	if( A.length == 0 )	return true;
+    	int len = A.length;
+    	int MaxDistance = 0;
+    	for( int i = 0; i < len; i++ ) {
+    		if( MaxDistance < i )
+    			return false;
+    		MaxDistance = Math.max(MaxDistance, i + A[i]);
+    		if( MaxDistance >= len - 1 )
+    			return true;
+    	}
+    	return false;
+    }
 }
