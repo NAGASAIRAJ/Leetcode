21f4728ac97c32d103a2234c6fc4276067372fb3
Yao <myspiritcrazy@gmail.com>
2013-12-06 22:33:45 -0500

    compare first and last digit of the target

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -19,4 +19,29 @@ public class PalindromeNumber {
     		result *= (-1);
     	return result;    		
     }
+    public boolean isPalindrome2(int x) {
+    	if( x < 0 )	return false;
+    	int x_tmp = x, TopDivider = 1;
+    	int TopDigit = 0, LastDigit = 0;
+    	boolean flag = true;
+    	
+    	while( x_tmp / 10 > 0 ) {
+    		TopDivider *= 10;
+    		x_tmp /= 10;
+    	}
+    	
+    	x_tmp = x;
+    	
+    	while( x_tmp > 0 ) {
+    		TopDigit = x_tmp / TopDivider;
+    		LastDigit = x_tmp % 10;
+    		if( TopDigit != LastDigit ) {
+    			flag = false;
+    			break;
+    		}
+    		x_tmp = x_tmp % TopDivider / 10;
+    	}
+    	
+    	return flag;
+    }
 }
