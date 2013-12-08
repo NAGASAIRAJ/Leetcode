6c3b6d79eb3727126c3cbcc9e5c190043a607fbf
Yao <myspiritcrazy@gmail.com>
2013-12-08 18:48:38 -0500

    scan forward and Count and Create string

---
+++ src/CountandSay.java
@@ -0,0 +1,22 @@
+
+public class CountandSay {
+    public String countAndSay(int n) {
+    	String result = "1";
+    	for( int i = 1; i < n; i++ )
+    		result = CountAndCreate(result);
+    	return result;
+    }
+    private String CountAndCreate(String result) {
+    	String tmp = "";
+    	int last = 0, len = result.length();
+    	for( int i = 0; i < len; i++ ) {
+    		if( result.charAt(i) != result.charAt(last) ) {
+    			tmp = tmp + (i - last) + result.charAt(last);
+    			last = i; 
+    		}
+    	}
+    	if( last < len )
+    		tmp = tmp + (len - last) + result.charAt(last);
+    	return result;
+    }
+}
