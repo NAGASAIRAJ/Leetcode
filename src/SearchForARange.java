6c59c2f5ba4658a3687e047701129e46c5555bcd
Yao <myspiritcrazy@gmail.com>
2013-12-07 23:36:44 -0500

    binary search twice to ensure worst case in O(logn)

--- src/SearchForARange.java
+++ src/SearchForARange.java
@@ -30,4 +30,35 @@ public class SearchForARange {
     	}
     	return -1;
     }
+    
+    public int[] searchRange2(int[] A, int target) {
+    	int l = 0, r = A.length - 1;
+    	int[] result = {-1, -1};
+    	while( l < r ) { // lower bound in l
+    		int mid = l + (r - l) / 2;
+    		if( target <= A[mid] )
+    			r = mid;
+    		else 
+    			l = mid + 1;    		
+    	}
+    	
+    	if( A[l] == target )
+    		result[0] = l;
+    	else 
+    		return result;
+    	l = 0;
+    	r = A.length;
+    	
+    	while( l < r) { // upper bound in r - 1
+    		int mid = l + (r - l) / 2;
+    		if( target >= A[mid] )
+    			l = mid + 1;
+    		else 
+    			r = mid;    		
+    		
+    	}
+    	
+    	result[1] = r - 1;
+    	return result;
+    }
 }
