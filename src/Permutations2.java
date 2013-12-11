1206b784f807b4eed8af6b4071b4addc2d40b130
Yao <myspiritcrazy@gmail.com>
2013-12-11 12:12:17 -0500

    from current depth to start generating Permutations

--- src/Permutations2.java
+++ src/Permutations2.java
@@ -6,25 +6,25 @@ public class Permutations2 {
 	public ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
 		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
 		if( num.length == 0 )	return result;
-		HashSet<ArrayList<Integer>> TmpPer = new HashSet<ArrayList<Integer>>();
-		GeneratePermutation(0, num.length, num, TmpPer, result);
+		HashSet<ArrayList<Integer>> TmpResult = new HashSet<ArrayList<Integer>>();
+		GeneratePermutation(0, num.length, num, TmpResult, result);
 		return result;
     }
-	private void GeneratePermutation(int depth, int len, int[] num, HashSet<ArrayList<Integer>> TmpPer,
+	private void GeneratePermutation(int depth, int len, int[] num, HashSet<ArrayList<Integer>> TmpResult,
 										ArrayList<ArrayList<Integer>> result) {
 		if( depth == len ) {
 			ArrayList<Integer> per = new ArrayList<Integer>();
 			for( int i = 0; i < len; i++ )
 				per.add(num[i]);
-			if( TmpPer.add(per) )
+			if( TmpResult.add(per) )
 				result.add(per);
 		}
-		for( int i = depth + 1; i < len; i++ ) {
+		for( int i = depth; i < len; i++ ) {
 			int tmp = num[depth];
 			num[depth] = num[i];
 			num[i] = tmp;
 			
-			GeneratePermutation(depth + 1, len, num, TmpPer, result);
+			GeneratePermutation(depth + 1, len, num, TmpResult, result);
 			
 			tmp = num[depth];
 			num[depth] = num[i];
