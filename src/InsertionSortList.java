4f89338374bc8fe9fc51d2d90aa8361f46984517
Yao <myspiritcrazy@gmail.com>
2013-12-11 14:48:16 -0500

    use two list node pointer to find appropriate plact to insert node

--- src/InsertionSortList.java
+++ src/InsertionSortList.java
@@ -1,4 +1,28 @@
 
 public class InsertionSortList {
-
+    public ListNode insertionSortList(ListNode head) {
+    	if( head == null )	return head;
+    	ListNode TmpHead = new ListNode(0);
+    	ListNode NewList = TmpHead, OldList = head;
+    	
+    	while( OldList != null ) {
+    		ListNode pre = FindInsertPosition(NewList, OldList);
+    		ListNode OldNext = OldList.next;
+    		OldList.next = pre.next;
+    		pre.next = OldList;
+    		OldList = OldNext;
+    	}
+    	
+    	return TmpHead.next;    
+    }    
+    private ListNode FindInsertPosition(ListNode NewList, ListNode OldList) {
+    	ListNode pre = NewList, cur = NewList.next;
+    	
+    	while( cur != null && cur.val <= OldList.val ) {
+    		pre = cur;
+    		cur = cur.next;
+    	}
+    	
+    	return pre;
+    }
 }
