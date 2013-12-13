789079f847689d822d7149447133ad3a41593fc2
Yao <myspiritcrazy@gmail.com>
2013-12-12 22:56:37 -0500

    check visiting cicurlar Gas Station or not in dynamic programming

--- src/GasStation.java
+++ src/GasStation.java
@@ -1,4 +1,18 @@
 
 public class GasStation {
-
+	public int canCompleteCircuit(int[] gas, int[] cost) {
+		int CurSum = 0, TotalCost = 0, index = -1;
+		for( int i = 0; i < gas.length; i++ ) {
+			CurSum += (gas[i] - cost[i]);
+			TotalCost += (gas[i] - cost[i]);
+			if( CurSum < 0 ) {
+				index = i;
+				CurSum = 0;
+			}
+		}
+		if( TotalCost >= 0 )
+			return (index + 1);
+		else 
+			return -1;	
+    }
 }
