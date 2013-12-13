73bdefc9b0a9866937958fc84f5e655806120d2c
Yao <myspiritcrazy@gmail.com>
2013-12-12 21:51:10 -0500

    recursively map digit to letters and get Letter Combinations of a Phone Number

---
+++ src/LetterCombinationsofaPhoneNumber.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+
+public class LetterCombinationsofaPhoneNumber {
+    public ArrayList<String> letterCombinations(String digits) {
+    	ArrayList<String> coms = new ArrayList<String>();
+    	if( digits.length() == 0 || digits == null )
+    		return coms;
+    	StringBuilder com = new StringBuilder();
+    	String[] board = new String[] {" ", "1", "abc", "def", 
+    									"ghi", "jkl", "mno", "pqrs", 
+    									"tuv", "wxyz"};
+    	DigitToLetters(0, digits, board, com, coms);
+    	return coms;
+    }
+    private void DigitToLetters(int depth, String digits, String[] board,  
+    							StringBuilder com, ArrayList<String> coms) {
+    	if( depth == digits.length() ) {
+    		coms.add(com.toString());
+    		return;
+    	}
+    	String s = board[digits.charAt(depth) - '0'];
+    	for( int i = 0; i < s.length(); i++ ) {
+    		com.append(s.charAt(i));
+    		DigitToLetters(depth + 1, digits, board, com, coms);
+    		com.deleteCharAt(depth);
+    	}
+    }
+}
