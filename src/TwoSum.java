891f4557e4b6076eea36af7420f3819cdc206301
Yao <myspiritcrazy@gmail.com>
2013-12-14 15:43:34 -0500

    update the final return value

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -10,7 +10,7 @@ public class TwoSum {
 		int l = 0, r = numbers.length - 1;
 		while( l < r ) {
 			if( numbers[l] + numbers[r] == target )
-				return new int[]{l, r};
+				return new int[]{l + 1, r + 1};
 			else if( numbers[l] + numbers[r] < target )
 				l++;
 			else
