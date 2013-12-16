cd8f172d111305dc7e36ca6c94e177b3646010cf
Yao <myspiritcrazy@gmail.com>
2013-12-15 22:35:32 -0500

    use local variable rather than parameter passed to process in local method

--- src/LongestPalindromicSubstring.java
+++ src/LongestPalindromicSubstring.java
@@ -1,9 +1,9 @@
 
 public class LongestPalindromicSubstring {
 	public static void main(String[] args) {
-		String s = "bb";
+		//String s = "bb";
 		String s2 = "abb";
-		System.out.println(longestPalindrome(s));
+		//System.out.println(longestPalindrome(s));
 		System.out.println(longestPalindrome(s2));
 	}
 	
@@ -12,19 +12,21 @@ public class LongestPalindromicSubstring {
 		String LongestStr = "";
 		for( int i = 0; i < s.length() - 1; i++ ) {
 			String OddStr = ExpandAroundCenter(s, i, i);
+			System.out.println(OddStr);
 			if( OddStr.length() > LongestStr.length() )
 				LongestStr = OddStr;
 			String EvenStr = ExpandAroundCenter(s, i, i + 1);
+			System.out.println(EvenStr);
 			if( EvenStr.length() > LongestStr.length() )
 				LongestStr = EvenStr;			
 		}
-		return LongestStr.toString();
+		return LongestStr;
     }
 	private static String ExpandAroundCenter(String s, int l, int r) {
 		int l_tmp = l, r_tmp = r;
 		boolean FirstTime = true;
 
-		while( l_tmp >= 0 && r_tmp < s.length() && s.charAt(l) == s.charAt(r) ) {
+		while( l_tmp >= 0 && r_tmp < s.length() && s.charAt(l_tmp) == s.charAt(r_tmp) ) {
 		    if( FirstTime )
 		        FirstTime = false;
 			l_tmp--;
@@ -35,6 +37,9 @@ public class LongestPalindromicSubstring {
 			l_tmp++;
 			r_tmp--;
 		}
+		else
+			r_tmp--;
+		
 		return s.substring(l_tmp, r_tmp + 1);
 	}
 }
