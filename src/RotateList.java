e828c4eb7692203cb0bbd7685f551973c0b46319
Yao <myspiritcrazy@gmail.com>
2013-12-17 17:33:34 -0500

    connect end with head then break the list in the appropriate position

--- src/RotateList.java
+++ src/RotateList.java
@@ -1,6 +1,34 @@
 
 public class RotateList {
 	public ListNode rotateRight(ListNode head, int n) {
-        
+		if( head == null )	return head;
+		int len = 1;
+		ListNode it = head;
+		
+		while( it.next != null ) {
+			len++;
+			it = it.next;
+		}
+		
+		it.next = head;
+		int start = (len - n) % len;
+		
+		while( start < 0 ) {
+			start += len;
+			start %= len;
+		}
+		
+		it = head;
+		start--;
+		
+		while( start > 0 ) {
+			it = it.next;
+			start--;
+		}
+		
+		head = it.next;
+		it.next = null;
+		
+		return head;
     }
 }
