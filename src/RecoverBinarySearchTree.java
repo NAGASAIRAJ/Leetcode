5a6361925fde6bb5ebeb2e4f368e30a1fc012df1
Yao <myspiritcrazy@gmail.com>
2013-12-18 10:26:10 -0500

    use Threaded Binary Tree to traversal tree without stack

--- src/RecoverBinarySearchTree.java
+++ src/RecoverBinarySearchTree.java
@@ -22,4 +22,55 @@ public class RecoverBinarySearchTree {
     	if( root.right != null )
     		InorderTraversal(root.right, nodes, values);
     }
+    
+    public void recoverTree2(TreeNode root) {
+    	if( root == null )	return;
+    	TreeNode f1 = null, f2 = null;
+    	TreeNode cur, pre, parent = null;
+    	boolean found = false;
+    	cur = root;
+    	
+    	while( cur != null ) {
+    		if( cur.left == null ) {
+    			if( parent != null && parent.val > cur.val ) {
+    				if( !found ) {
+    					f1 = parent;
+    					found = true;
+    				}
+    				f2 = cur;
+    			}
+    			parent = cur;
+        		cur = cur.right;
+    		}    		
+    		else {
+    			pre = cur.left;
+    			
+    			while( pre.right != null && pre.right != cur ) 
+    				pre = pre.right;
+    			
+    			if( pre.right == null ) {
+    				pre.right = cur;
+    				cur = cur.left;
+    			}
+    			else {
+    				pre.right = null;
+    				if( parent.val > cur.val ) {
+    					if( !found ) {
+    						f1 = parent;
+    						found = true;
+    					}
+    					f2 = cur;
+    				}
+    				parent = cur;
+    				cur = cur.right;
+    			}    				
+    		}
+    	}
+    	
+    	if( f1 != null && f2 != null ) {
+    		int tmp = f1.val;
+    		f1.val = f2.val;
+    		f2.val = tmp;
+    	}
+    }
 }
