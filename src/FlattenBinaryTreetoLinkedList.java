a5be28b7e066450508a76fb7cbf754fecace6044
Yao <myspiritcrazy@gmail.com>
2014-01-06 10:53:49 -0500

    add comment

---
+++ src/FlattenBinaryTreetoLinkedList.java
@@ -0,0 +1,18 @@
+
+public class FlattenBinaryTreetoLinkedList {
+    public void flatten(TreeNode root) {
+    	if( root == null )	return;
+    	flatten(root.left);
+    	flatten(root.right);
+    	TreeNode tmp = root;
+    	if( tmp.left == null )	return; // flattening is done
+    	else tmp = tmp.left;
+    	
+    	while( tmp.right != null )
+    		tmp = tmp.right;
+    	
+    	tmp.right = root.right;
+    	root.right = root.left;
+    	root.left = null;    	
+    }
+}
