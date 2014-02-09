6104b1cdea82b53f3ec31e75923162d7618efbf2
Yao <myspiritcrazy@gmail.com>
2014-02-09 15:39:39 -0800

    add a recursive solution even though time limit exceeded(TLE)

--- src/ValidPalindrome.java
+++ src/ValidPalindrome.java
@@ -22,4 +22,18 @@ public class ValidPalindrome {
     	}
     	return true;
     }
+    
+    public boolean isPalindrome2(String word) {
+        //Strip out non-alphanumeric characters from string
+        String cleanWord = word.replaceAll("[^a-zA-Z0-9]","");
+        //Check for palindrome quality recursively
+        return checkPalindrome(cleanWord.toLowerCase());
+    }
+    private boolean checkPalindrome(String word) {
+        if(word.length() < 2) { return true;  }
+        char first  = word.charAt(0);
+        char last   = word.charAt(word.length()-1);
+        if(  first != last  ) { return false; }
+        else { return checkPalindrome(word.substring(1,word.length()-1)); }
+    }
 }
