3dfe0a13a5b543ebb582db925b6d004d67f1060d
Yao <myspiritcrazy@gmail.com>
2014-02-08 23:05:25 -0800

    add 1D DP array solution

--- src/DistinctSubsequences.java
+++ src/DistinctSubsequences.java
@@ -24,4 +24,15 @@ public class DistinctSubsequences {
 		}
 		return NumDisSub[SLen][TLen];
 	}
+	
+	public int numDistinct2(String S, String T) {
+        int[] f = new int[T.length() + 1];
+        f[T.length()] = 1;
+        for( int i = S.length() - 1; i >= 0; i-- ) {
+            for(int j = 0; j < T.length(); j++) {
+                f[j] += (S.charAt(i) == T.charAt(j))?  f[j+1] : 0;
+            }
+        }
+        return f[0];
+    }
 }
