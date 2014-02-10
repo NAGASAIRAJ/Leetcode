87fbd4f0bbd91e20cafd4d2978b4a0ad93bb84e9
Yao <myspiritcrazy@gmail.com>
2014-02-09 16:07:37 -0800

    refactor code

--- src/RestoreIPAddresses.java
+++ src/RestoreIPAddresses.java
@@ -35,27 +35,27 @@ public class RestoreIPAddresses {
 	
 	public ArrayList<String> restoreIpAddresses2(String s) {
         ArrayList<String> res = new ArrayList<String>();
-        if (s.length()<4||s.length()>12) return res;
-        dfs(s,"",res,0);
+        if( s.length() < 4 || s.length() > 12 ) return res;
+        dfs(s, "", res, 0);
         return res;
     }
     
-    public void dfs(String s, String tmp, ArrayList<String> res, int count){
-        if (count == 3 && isValid(s)) {
+    public void dfs(String s, String tmp, ArrayList<String> res, int depth){
+        if( depth == 3 && isValid(s) ) {
             res.add(tmp + s);
             return;
         }
-        for(int i=1; i<4 && i<s.length(); i++){
-            String substr = s.substring(0,i);
-            if (isValid(substr)){
-                dfs(s.substring(i), tmp + substr + '.', res, count+1);
+        for( int i = 1; i < 4 && i < s.length(); i++ ){
+            String substr = s.substring(0, i);
+            if( isValid(substr) ){
+                dfs(s.substring(i), tmp + substr + '.', res, depth + 1);
             }
         }
     }
     
     public boolean isValid(String s){
-        if (s.charAt(0)=='0') return s.equals("0");
+        if( s.charAt(0) == '0' ) return s.equals("0");
         int num = Integer.parseInt(s);
-        return num<=255 && num>0;
+        return num <= 255 && num > 0;
     }
 }
