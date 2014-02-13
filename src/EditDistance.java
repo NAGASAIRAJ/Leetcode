21158536c6357329e7276c4853f99ff7c1394a53
Yao <myspiritcrazy@gmail.com>
2014-02-13 11:25:21 -0800

    update commnet

--- src/EditDistance.java
+++ src/EditDistance.java
@@ -13,8 +13,8 @@ public class EditDistance {
     				if( word1.charAt(i - 1) == word2.charAt(j - 1))
     					ed[i][j] = ed[i - 1][j - 1];
     				else 
-    					// replace ch1 with ch2: 1+ ed[i-1][j-1]
-    					// insert ch2 in word2(word1+ch1, word2): 1 + ed[i][j-1j]
+    					// replace ch1 with ch2(word1+ch2, word2+ch2): 1+ ed[i-1][j-1]
+    					// insert ch2 in word1(word1+ch1+ch2, word2+ch2): 1 + ed[i][j-1]
     					// delete ch1 from word1(word1, word2+ch2): 1 + ed[i-1][j]
     					ed[i][j] = 1 + Math.min(ed[i - 1][j - 1], 
     							                Math.min(ed[i - 1][j], ed[i][j - 1]));
