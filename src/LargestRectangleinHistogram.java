b1ed421273408505c33413fec49196d77867a01a
Yao <myspiritcrazy@gmail.com>
2014-03-06 16:55:22 -0800

    add another solution with left and right expand array to calculate Largest Rectangle in Histogram

--- src/LargestRectangleinHistogram.java
+++ src/LargestRectangleinHistogram.java
@@ -24,4 +24,37 @@ public class LargestRectangleinHistogram {
 		
 		return area;
     }
+	
+	public int largestRectangleArea2(int[] height) {
+		int maxArea = 0;
+		int len = height.length;
+		int[] leftExpand = new int[len];
+		int[] rightExpand = new int[len];
+		for (int i = 0; i < len; i++) {
+			leftExpand[i] = 0;
+			rightExpand[i] = len - 1;
+		}
+		for (int i = 1; i < len; i++) {
+			if (height[i - 1] < height[i]) {
+				leftExpand[i] = i;
+			} else {
+				int j = i;
+				rightExpand[j - 1] = i - 1;
+				while (j > 0) {
+					if (height[j - 1] < height[i]) {
+						break;
+					} else {
+						rightExpand[j - 1] = i - 1;
+						j = leftExpand[j - 1];
+					}
+				}
+				leftExpand[i] = j;
+			}
+		}
+		for (int i = 0; i < len; i++) {
+			maxArea = Math.max(maxArea, 
+					(rightExpand[i] - leftExpand[i] + 1) * height[i]);
+		}
+		return maxArea;
+	}
 }
