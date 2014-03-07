647018a79d180bbed847d814d6511bf47ffe4e56
Yao <myspiritcrazy@gmail.com>
2014-03-07 01:18:00 -0800

    trim space in the end

--- src/ReverseWordsinaString.java
+++ src/ReverseWordsinaString.java
@@ -10,6 +10,16 @@ public class ReverseWordsinaString {
 		//System.out.println("Reversed partially: " + head + s.substring(end + 1));
 		return head; 
 	}
+	
+	private static String trimSpace(String s) {
+		int spaceIdx = s.length() - 1;
+		
+		while (spaceIdx >= 0 && s.charAt(spaceIdx) == ' ') {
+			spaceIdx--;
+		}
+		return s.substring(0, spaceIdx + 1);
+	}
+	
     public static String reverseWords(String s) {
         String res = "";
         if (s == " ")
@@ -25,26 +35,36 @@ public class ReverseWordsinaString {
         if (s.length() <= 1)
     		return s;
         String reversedWhole = reverse(s);
-        //System.out.println("Reversed whole: " + reversedWhole);
+        System.out.println("Reversed whole: " + reversedWhole);
         int start = 0;
         for (int i = 0; i < s.length(); i++) {
         	if (reversedWhole.charAt(i) == ' ') {
         		if (start + 1 < i) {
         			res += reverse(reversedWhole, start, i - 1);
+        		} else {
+        			res += ("" + reversedWhole.charAt(start));
         		}
         		start = i + 1;
         		res += " ";
         	}
         }
         
-        if (reversedWhole.charAt(s.length() - 1) != ' ')
+        if (reversedWhole.charAt(s.length() - 1) != ' ') {
         	res += reverse(reversedWhole, start, s.length() - 1);
+        } else {
+        	res = trimSpace(reversedWhole);
+        }
         return res;
     }
     public static void main(String[] args) {
     	 String s = "the sky is blue";
+    	 String s2 = " 1";
     	 System.out.println("Test case 1: ");
     	 System.out.println("Before reverse: " + s);
     	 System.out.println("After reverse: " + reverseWords(s));
+    	 
+    	 System.out.println("Test case 2: ");
+    	 System.out.println("Before reverse: " + s2);
+    	 System.out.println("After reverse: " + reverseWords(s2));
     }
 }
