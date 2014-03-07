ebd68170588924789d1c78d9dc822dec6ee29466
Yao <myspiritcrazy@gmail.com>
2014-03-07 00:49:21 -0800

    reverse words in a string

---
+++ src/ReverseWordsinaString.java
@@ -0,0 +1,39 @@
+
+public class ReverseWordsinaString {
+	private static String reverse(String s) {
+		StringBuilder tmp = new StringBuilder(s);
+		tmp.reverse();
+		return tmp.toString();
+	}
+	private static String reverse(String s, int start, int end) {
+		String head = reverse(s.substring(start, end + 1));
+		//System.out.println("Reversed partially: " + head + s.substring(end + 1));
+		return head; 
+	}
+    public static String reverseWords(String s) {
+    	if (s.length() <= 1)
+    		return s;
+        String reversedWhole = reverse(s);
+        //System.out.println("Reversed whole: " + reversedWhole);
+        int start = 0;
+        String res = "";
+        for (int i = 0; i < s.length(); i++) {
+        	if (reversedWhole.charAt(i) == ' ') {
+        		if (start + 1 < i) {
+        			res += reverse(reversedWhole, start, i - 1);
+        		}
+        		start = i + 1;
+        		res += " ";
+        	}
+        }
+        if (reversedWhole.charAt(s.length() - 1) != ' ')
+        	res += reverse(reversedWhole, start, s.length() - 1);
+        return res;
+    }
+    public static void main(String[] args) {
+    	 String s = "the sky is blue";
+    	 System.out.println("Test case 1: ");
+    	 System.out.println("Before reverse: " + s);
+    	 System.out.println("After reverse: " + reverseWords(s));
+    }
+}
