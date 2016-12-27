e57e97ed88a4b207a2d02e8d4548456297ddce1a
Yao <myspiritcrazy@gmail.com>
2016-12-27 00:57:38 -0800

    first commit for surrounded regions

---
+++ in_python/surrounded_regions.py
@@ -0,0 +1,46 @@
+class Solution(object):
+    def solve(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: void Do not return anything, modify board in-place instead.
+        """
+        w, h = len(board[0]), len(board)
+        visited = [[0 for x in range(w)] for y in range(h)]
+        for y in range(w):
+            if board[0][y] == 'O':
+                self.helper_func(0, y, visited, board)
+            if board[h-1][y] == 'O':
+                self.helper_func(h-1, y, visited, board)
+
+        for x in range(h):
+            if board[x][0] == 'O':
+                self.helper_func(0, y, visited, board)
+            if board[x][w-1] == 'O':
+                self.helper_func(h-1, y, visited, board)
+
+        for x in range(h):
+            for y in range(w):
+                if board[x][y] == 'O':
+                    board[x][y] = 'X'
+                elif board[x][y] == 'P':
+                    board[x][y] = 'O'
+
+        return board
+
+    def helper_func(self, x, y, visited, board):
+        if x < 0 or y < 0 or visited[x][y] == 1 or board[x][y] == 'X':
+            return
+        else:
+            visited[x][y] = 1
+            board[x][y] = 'P'
+            self.helper_func(x-1, y, visited, board)
+            self.helper_func(x+1, y, visited, board)
+            self.helper_func(x, y-1, visited, board)
+            self.helper_func(x, y+1, visited, board)
+
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    board = [['X', 'X', 'X', 'X'], ['X', 'O', 'O', 'X'], ['X', 'X', 'O', 'X'], ['X', 'O', 'X', 'X']]
+    print sol.solve(board)
