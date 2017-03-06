983d3e444e851e924ed5d49e3c729c4608125a3d
Yao <myspiritcrazy@gmail.com>
2017-03-05 23:00:32 -0800

    refactor code for better format

--- in_python/word_break_2.py
+++ in_python/word_break_2.py
@@ -1,157 +1,163 @@
 class Solution(object):
-    def wordBreak(self, s, wordDict):
+    def word_break(self, s, word_dict):
         """
         :type s: str
-        :type wordDict: List[str]
+        :type word_dict: List[str]
         :rtype: List[str]
         """
-        curWordSet = set()
+        cur_word_set = set()
         result = []
-        sLen = len(s)
-        if sLen == 1:
-            if s in wordDict:
+        s_len = len(s)
+        if s_len == 1:
+            if s in word_dict:
                 result.append(s)
                 return result
             else:
                 return result
         else:
-            if self.checkChar(s, sLen, wordDict):
-                cacheBuffer = {}
-                return self.wordBreak2(s, sLen, 0, curWordSet, wordDict, "", "", cacheBuffer, result)
-                # return self.wordBreak3(s, sLen, 0, wordDict, "", "", result)
+            if self.check_char(s, s_len, word_dict):
+                cache_buffer = {}
+                return self.word_break_2(s, s_len, 0, cur_word_set, word_dict, "", "", cache_buffer, result)
+                # return self.wordBreak3(s, s_len, 0, word_dict, "", "", result)
             else:
                 return result
         
-    def wordBreak2(self, inputStr, strLen, depth, curWordSet, wordDict, tmpWord, tmpRes, cacheBuffer, result):
+    def word_break_2(self, input_str, str_len, depth, cur_word_set, word_dict, tmp_word, tmp_res, cache_buffer, result):
         """
-        :type inputStr: str
-        :type strLen: int
+        :type input_str: str
+        :type str_len: int
         :type depth: int
-        :type wordDict: Set[str]
-        :type tmpWord: str
-        :type tmpRes: str
-        :type wordDict: Set[str]
+        :type cur_word_set Set[str]
+        :type word_dict: Set[str]
+        :type tmp_word: str
+        :type tmp_res: str
+        :type cache_buffer: dict
+        :type word_dict: Set[str]
+        :type result: List[str]
         :rtype result: List[str]
         """
-        print "Processing depth: %d" %(depth)
-        if inputStr[depth:] in cacheBuffer:
-            print "Depth: %d" %(depth)
-            print "later str: " + inputStr[depth:]
+        print "Processing depth: %d" % depth
+        if input_str[depth:] in cache_buffer:
+            print "Depth: %d" % depth
+            print "later str: " + input_str[depth:]
             print "Cache: "
-            for key in cacheBuffer:
-                print (key, ':', cacheBuffer[key])
-            if depth == (strLen - 1):
-                if not tmpRes:
-                    result.append(cacheBuffer[inputStr[depth:]])
-                elif (tmpRes[1:] + " " + cacheBuffer[inputStr[depth:]]) not in result:
-                    result.append(tmpRes[1:] + " " + cacheBuffer[inputStr[depth:]])
+            for key in cache_buffer:
+                print (key, ':', cache_buffer[key])
+            if depth == (str_len - 1):
+                if not tmp_res:
+                    result.append(cache_buffer[input_str[depth:]])
+                elif (tmp_res[1:] + " " + cache_buffer[input_str[depth:]]) not in result:
+                    result.append(tmp_res[1:] + " " + cache_buffer[input_str[depth:]])
             else:
-                print "cached, tmpWord: " + cacheBuffer[inputStr[depth:]]
-                print "cached, tmpRes: " + tmpRes
-                if not tmpRes:
-                    tmpResult = cacheBuffer[inputStr[depth:]]
+                print "cached, tmp_word: " + cache_buffer[input_str[depth:]]
+                print "cached, tmp_res: " + tmp_res
+                if not tmp_res:
+                    tmp_result = cache_buffer[input_str[depth:]]
                 else:
-                    tmpResult = tmpRes[1:] + " " + cacheBuffer[inputStr[depth:]]
-                if tmpResult not in result:
-                    result.append(tmpResult)
-                tmpWord = inputStr[depth]
-                if tmpWord in curWordSet or tmpWord in wordDict:
-                    curWordSet.add(tmpWord)
-                    if not tmpRes:
-                        cacheBuffer[inputStr[0:depth + 1]] = tmpWord
+                    tmp_result = tmp_res[1:] + " " + cache_buffer[input_str[depth:]]
+                if tmp_result not in result:
+                    result.append(tmp_result)
+                tmp_word = input_str[depth]
+                if tmp_word in cur_word_set or tmp_word in word_dict:
+                    cur_word_set.add(tmp_word)
+                    if not tmp_res:
+                        cache_buffer[input_str[0:depth + 1]] = tmp_word
                     else:
-                        cacheBuffer[inputStr[0:depth + 1]] = (tmpRes[1:] + " " + tmpWord)
-                    self.wordBreak2(inputStr, strLen, depth + 1, curWordSet, wordDict, "", tmpRes + " " + tmpWord, cacheBuffer,
-                                result)
-                    del cacheBuffer[inputStr[0:depth + 1]]
+                        cache_buffer[input_str[0:depth + 1]] = (tmp_res[1:] + " " + tmp_word)
+                    self.word_break_2(input_str, str_len, depth + 1, cur_word_set, word_dict, "", tmp_res + " "
+                                      + tmp_word, cache_buffer, result)
+                    del cache_buffer[input_str[0:depth + 1]]
                 return result
 
-        for i in range(depth, strLen):
-            tmpWord += inputStr[i]
-            if tmpWord in curWordSet or tmpWord in wordDict:
-                if i == (strLen - 1):
+        for i in range(depth, str_len):
+            tmp_word += input_str[i]
+            if tmp_word in cur_word_set or tmp_word in word_dict:
+                if i == (str_len - 1):
                     print "Last step"
-                    print "tmpWord: " + tmpWord
-                    print "tmpRes: " + tmpRes
-                    if not tmpRes:
-                        if tmpWord not in result:
-                            result.append(tmpWord)
-                    elif (tmpRes[1:] + " " + tmpWord) not in result:
-                        if (tmpRes[1:] + " " + tmpWord) not in result:
-                            result.append(tmpRes[1:] + " " + tmpWord)
+                    print "tmp_word: " + tmp_word
+                    print "tmp_res: " + tmp_res
+                    if not tmp_res:
+                        if tmp_word not in result:
+                            result.append(tmp_word)
+                    elif (tmp_res[1:] + " " + tmp_word) not in result:
+                        if (tmp_res[1:] + " " + tmp_word) not in result:
+                            result.append(tmp_res[1:] + " " + tmp_word)
                     break
                 else:
-                    print "Step %d" %(i)
-                    curWordSet.add(tmpWord)
-                    if not tmpRes:
-                        cacheBuffer[inputStr[0:i+1]] = tmpWord
+                    print "Step %d" % i
+                    cur_word_set.add(tmp_word)
+                    if not tmp_res:
+                        cache_buffer[input_str[0:i+1]] = tmp_word
                     else:
-                        cacheBuffer[inputStr[0:i+1]] = (tmpRes[1:] + " " + tmpWord)
-                    print "Depth: %d" % (depth)
+                        cache_buffer[input_str[0:i+1]] = (tmp_res[1:] + " " + tmp_word)
+                    print "Depth: %d" % depth
                     print "Before del, Cache: "
-                    for key in cacheBuffer:
-                        print (key, ':', cacheBuffer[key])
-                    print "tmpWord: " + tmpWord
-                    print "tmpRes: " + tmpRes
-                    if tmpWord in wordDict:
-                        self.wordBreak2(inputStr, strLen, i+1, curWordSet, wordDict, "", tmpRes + " " + tmpWord, cacheBuffer, result)
+                    for key in cache_buffer:
+                        print (key, ':', cache_buffer[key])
+                    print "tmp_word: " + tmp_word
+                    print "tmp_res: " + tmp_res
+                    if tmp_word in word_dict:
+                        self.word_break_2(input_str, str_len, i + 1, cur_word_set, word_dict, "", tmp_res + " "
+                                          + tmp_word, cache_buffer, result)
                     else:
-                        self.wordBreak2(inputStr, strLen, i + 1, curWordSet, wordDict, tmpWord, tmpRes, cacheBuffer, result)
-                    print "tmpWord: " + tmpWord
-                    print "tmpRes: " + tmpRes
-                    del cacheBuffer[inputStr[0:i+1]]
-                    print "Depth: %d" % (depth)
+                        self.word_break_2(input_str, str_len, i + 1, cur_word_set, word_dict, tmp_word, tmp_res,
+                                          cache_buffer, result)
+                    print "tmp_word: " + tmp_word
+                    print "tmp_res: " + tmp_res
+                    del cache_buffer[input_str[0:i+1]]
+                    print "Depth: %d" % depth
                     print "After del, Cache: "
-                    for key in cacheBuffer:
-                        print (key, ':', cacheBuffer[key])
+                    for key in cache_buffer:
+                        print (key, ':', cache_buffer[key])
 
         return result
 
-    def wordBreak3(self, inputStr, strLen, depth, wordDict, tmpWord, tmpRes, result):
+    def word_break_3(self, input_str, str_len, depth, word_dict, tmp_word, tmp_res, result):
         """
-        :type inputStr: str
-        :type strLen: int
+        :type input_str: str
+        :type str_len: int
         :type depth: int
-        :type wordDict: Set[str]
-        :type tmpWord: str
-        :type tmpRes: str
-        :type wordDict: Set[str]
+        :type word_dict: Set[str]
+        :type tmp_word: str
+        :type tmp_res: str
+        :type word_dict: Set[str]
+        :type result: List[str]
         :rtype result: List[str]
         """
-        print "Processing depth: %d" %(depth)
-        for i in range(depth, strLen):
-            tmpWord += inputStr[i]
-            if i == (strLen - 1):
+        print "Processing depth: %d" % depth
+        for i in range(depth, str_len):
+            tmp_word += input_str[i]
+            if i == (str_len - 1):
                 print "Last step"
-                print "tmpWord: " + tmpWord
-                print "tmpRes: " + tmpRes
-                if tmpWord in wordDict:
-                    if not tmpRes:
-                        if tmpWord not in result:
-                            result.append(tmpWord)
-                    elif (tmpRes[1:] + " " + tmpWord) not in result:
-                        if (tmpRes[1:] + " " + tmpWord) not in result:
-                            result.append(tmpRes[1:] + " " + tmpWord)
+                print "tmp_word: " + tmp_word
+                print "tmp_res: " + tmp_res
+                if tmp_word in word_dict:
+                    if not tmp_res:
+                        if tmp_word not in result:
+                            result.append(tmp_word)
+                    elif (tmp_res[1:] + " " + tmp_word) not in result:
+                        if (tmp_res[1:] + " " + tmp_word) not in result:
+                            result.append(tmp_res[1:] + " " + tmp_word)
                     break
             else:
-                print "tmpWord: " + tmpWord
-                print "tmpRes: " + tmpRes
-                if tmpWord in wordDict:
-                    self.wordBreak3(inputStr, strLen, i+1, wordDict, "", tmpRes + " " + tmpWord, result)
+                print "tmp_word: " + tmp_word
+                print "tmp_res: " + tmp_res
+                if tmp_word in word_dict:
+                    self.word_break_3(input_str, str_len, i + 1, word_dict, "", tmp_res + " " + tmp_word, result)
                 else:
-                    self.wordBreak3(inputStr, strLen, i + 1, wordDict, tmpWord, tmpRes, result)
-                print "tmpWord: " + tmpWord
-                print "tmpRes: " + tmpRes
+                    self.word_break_3(input_str, str_len, i + 1, word_dict, tmp_word, tmp_res, result)
+                print "tmp_word: " + tmp_word
+                print "tmp_res: " + tmp_res
         return result
 
-    def checkChar(self, inputStr, strLen, wordDict):
-        charSet = set()
-        for word in wordDict:
+    def check_char(self, input_str, str_len, word_dict):
+        char_set = set()
+        for word in word_dict:
             for i in range(len(word)):
-                charSet.add(word[i])
+                char_set.add(word[i])
 
-        for i in range(strLen):
-            if inputStr[i] in charSet:
+        for i in range(str_len):
+            if input_str[i] in char_set:
                 continue
             else:
                 return False
@@ -163,9 +169,10 @@ if __name__ == "__main__":
     # print sol.wordBreak("a", {"a"})
     # print sol.wordBreak("apple", {"pear","apple","peach"})
     # print sol.wordBreak("catsanddog", {"cat", "cats", "and", "sand", "dog"})
-    print sol.wordBreak("aggegbnngohbggalojckbdfjakgnnjadhganfdkefeddjdnabmflabckflfljafdlmmbhijojiaaifedaihnoinedhhnolcjdam",["o","b","gbdfgiokkfnhl","glibjohcmd","bblcnhelanckn","mflabckflflja","mgda","oheafhajjo","cc","cffalholojikojm","haljiamccabh","gjkdlonmhdacd","ee","bc","mjj","fdlmmbhij","nn","jiaaifedaihn","nhligg","hooaglldlei","hajhebh","ebijeeh","me","eibm","ekkobhajgkem","ohaofonhjakc","n","kjjogm","mhn","odcamjmodie","edmagbkejiocacl","kcbfnjialef","lhifcohoe","akgnn","fbgakjhjb","belggjekmn","oinedhhnolc","ddekcnag","oneoakldakalb","bodnokemafkhkhf","dkefeddjdnab","gflcngff","fgnfmbcogmojgm","ad","jadhganf","lojckbdfj","gadkaoe","jdam","ljjndlnednnombl","aggegbnngohbgga"])
+    print sol.word_break("aggegbnngohbggalojckbdfjakgnnjadhganfdkefeddjdnabmflabckflfljafdlmmbhijojiaaifedaihnoinedhhnolcjdam",
+                         ["o", "b", "gbdfgiokkfnhl", "glibjohcmd", "bblcnhelanckn", "mflabckflflja", "mgda", "oheafhajjo", "cc", "cffalholojikojm", "haljiamccabh", "gjkdlonmhdacd", "ee", "bc", "mjj", "fdlmmbhij", "nn", "jiaaifedaihn", "nhligg", "hooaglldlei", "hajhebh", "ebijeeh", "me", "eibm", "ekkobhajgkem", "ohaofonhjakc", "n", "kjjogm", "mhn", "odcamjmodie", "edmagbkejiocacl", "kcbfnjialef", "lhifcohoe", "akgnn", "fbgakjhjb", "belggjekmn", "oinedhhnolc", "ddekcnag", "oneoakldakalb", "bodnokemafkhkhf", "dkefeddjdnab", "gflcngff", "fgnfmbcogmojgm", "ad", "jadhganf", "lojckbdfj", "gadkaoe", "jdam", "ljjndlnednnombl", "aggegbnngohbgga"])
     # print sol.wordBreak("bb", {"a","b","bbb","bbbb"})
-    print sol.wordBreak("aaaaaaa", {"aaaa","aa","a"})
+    print sol.word_break("aaaaaaa", {"aaaa", "aa", "a"})
     # TODO: need debug when add cache
     # print sol.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", {"a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"})
     # print sol.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", {"aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa","ba"})
