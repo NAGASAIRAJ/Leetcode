61d4bbc3ad7e5e18caa8f1cbc112993a3183fff7
Yao <myspiritcrazy@gmail.com>
2017-05-06 10:07:08 -0700

    recursively get permutations from rest of array elements

--- src/Permutations.java
+++ src/Permutations.java
@@ -5,6 +5,13 @@ public class Permutations {
     public ArrayList<ArrayList<Integer>> permute(int[] num) {
     	ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
     	permutation(num, 0, num.length, result);
+
+        ArrayList<Integer> inputDigits = new ArrayList<Integer>();
+        for (int i = 0; i < num.length; i++) {
+            inputDigits.add(num[i]);
+        }
+        ArrayList<Integer> result1 = new ArrayList<Integer>(permutationsOf(inputDigits));
+
     	return result;
     }
     public void permutation(int[] num, int depth, int len, ArrayList<ArrayList<Integer>> result ) {
@@ -27,4 +34,21 @@ public class Permutations {
     		num[depth] = tmp;
     	}    	
     }
+    public static ArrayList<Integer> permutationsOf(ArrayList<Integer> inputDigits) {
+      if (inputDigits.size() == 1) {
+        return inputDigits;
+      }
+      ArrayList<Integer> result = new ArrayList<Integer>();
+      for (int i = 0; i < inputDigits.size(); i++) {
+        Integer digit = inputDigits.get(i);
+        ArrayList<Integer> restDigits = new ArrayList<Integer>(inputDigits);
+        restDigits.remove(digit);
+        ArrayList<Integer> permutationOfRestOfDigits = new ArrayList<Integer>(permutationsOf(restDigits));
+        for (int j = 0; j < permutationOfRestOfDigits.size(); j++) {
+          result.add(permutationOfRestOfDigits.get(j) * 10 + digit);
+        }
+     }
+   
+     return result;
+   }
 }
